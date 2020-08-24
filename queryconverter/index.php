7f46db6b4bef7a00d88c9f770fdc7fb79c852886
nikki <nikkimeadows1@gmail.com>
2020-08-24 16:52:59 +1200

    changed idea, 3rd vm reading from db now

--- queryconverter/index.php
+++ queryconverter/index.php
@@ -1,8 +1,31 @@
 <html>
 <head>
-<title>Testing...</title>
+<title>Result</title>
 </head>
 <body>
-<?php echo '<p>This is a test</p>'; ?> 
+
+
+
+    <table border="1">
+      <tr><th>First Name</th><th>Last Name</th><th>Height</th><th>Weight</th><th>Body Fat Percentage</th><th>Exercise Level</th><th>Goal</th></tr>
+
+<?php 
+
+$db_host   = '192.168.2.12';
+$db_name   = 'nikkidb';
+$db_user   = 'webserviceuser';
+$db_passwd = 'newpassword';
+
+$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+$q = $pdo->query("SELECT * FROM measurements");
+
+while($row = $q->fetch()){
+   echo "<tr><td>".$row["firstname"]."</td><td>".$row["lastname"]."</td><td>".$row["height"]."</td><td>".$row["weight"]."</td><td>".$row["bodyfat"]."</td><td>".$row["exerciselevel"]."</td><td>".$row["goal"]."</td></tr>\n";
+}
+
+?>
+</table> 
 </body>
 </html>
--- setup-database.sql
+++ setup-database.sql
@@ -1,7 +1,12 @@
-CREATE TABLE user (
-  firstname varchar(20),
-  lastname varchar(20)
+CREATE TABLE measurements (
+  firstname varchar(20) NOT NULL,
+  lastname varchar(50) NOT NULL,
+  height varchar(50) NOT NULL,
+  weight varchar(50) NOT NULL,	
+  bodyfat varchar(50) NOT NULL,
+  exerciselevel varchar(50) NOT NULL,
+  goal varchar (20)
 );
 
-INSERT INTO user VALUES ('Peter','Meadows');
-INSERT INTO user VALUES ('Nikki','Meadows');
+INSERT INTO measurements VALUES('Nikki', 'Meadows', '164', '62', '24', '3', '3');
+INSERT INTO measurements VALUES('Shaun', 'Drabble', '164', '62', '24', '3', '3');
